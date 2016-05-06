7edb0fb367afb05f57963f5bb4c4a4cf3aba1070
magnumjs <magnumjs@users.noreply.github.com>
2016-05-06 10:46:20 -0400

    Update spec.js

--- tests/spec.js
+++ tests/spec.js
@@ -1,9 +1,41 @@
+//Stubs: static  code so we don't test Redux
+
+Redux.createStore = Redux.applyMiddleware = function() {
+  return {
+    subscribe: function() {}
+  }
+};
 
 // specs code
 describe("Redux.connect", function() {
 
-  it("is defined", function() {
+  it("is defined as function", function() {
     expect(Redux.connect).toBeDefined();
+    expect(typeof Redux.connect).toBe('function');
+  });
+
+
+  describe("A spy", function() {
+    var component = {
+        render: function() {
+
+        }
+      },
+      hoc, params = {};
+
+    beforeEach(function() {
+
+      spyOn(Redux, 'connect').andCallThrough();
+      spyOn(component, 'render').andCallThrough();
+
+      hoc = Redux.connect(component);
+      hoc(params);
+    });
+
+    it("tracks that the spy was called", function() {
+      expect(Redux.connect).toHaveBeenCalled();
+      expect(typeof hoc).toBe('function');
+    });
   });
 
 });
