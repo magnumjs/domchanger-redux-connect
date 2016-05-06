9533c0239f535bced222385363b612ac18d9fcb6
magnumjs <magnumjs@users.noreply.github.com>
2016-05-06 11:15:07 -0400

    Update spec.js

--- tests/spec.js
+++ tests/spec.js
@@ -1,9 +1,12 @@
-//Stubs: static  code so we don't test Redux
+//Stubs: static code so we don't test Redux
 
 Redux.createStore = Redux.applyMiddleware = function() {
   return {
+    getState: function() {
+      return {};
+    },
     subscribe: function() {}
-  }
+  };
 };
 
 // specs code
@@ -17,24 +20,43 @@ describe("Redux.connect", function() {
 
   describe("A spy", function() {
     var component = {
-        render: function() {
-
-        }
+        update: function() {}
       },
-      hoc, params = {};
+      hoc, params = {},
+      wrappedComp, instance;
 
     beforeEach(function() {
 
       spyOn(Redux, 'connect').andCallThrough();
-      spyOn(component, 'render').andCallThrough();
 
       hoc = Redux.connect(component);
-      hoc(params);
+      wrappedComp = hoc(params);
+      spyOn(component, 'update').andCallThrough();
+
+      instance = wrappedComp.update(true);
     });
 
-    it("tracks that the spy was called", function() {
+    it("Redux.connect was called", function() {
       expect(Redux.connect).toHaveBeenCalled();
+
+    });
+
+    it("Redux.connect returns a HoC function", function() {
       expect(typeof hoc).toBe('function');
+    });
+    it("Redux.connect HoC function can be called with params", function() {
+      expect(wrappedComp).toBeDefined();
+    });
+    it("HoC function called returns an object with a store property", function() {
+      expect(typeof wrappedComp).toBe('object');
+      expect(wrappedComp.store).toBeDefined();
+    });
+    it("HoC function called returned Object can call update function", function() {
+      expect(component.update).toHaveBeenCalled();
+    });
+    it("update function returns the same instance with a store property", function() {
+      expect(typeof instance).toBe('object');
+      expect(instance.store).toBeDefined();
     });
   });
 
